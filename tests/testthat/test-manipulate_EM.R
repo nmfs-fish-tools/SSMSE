c6be6be9a1e4f6b971eb961378d5b23912361037
k-doering <kathryn.doering@noaa.gov>
2019-11-08 08:01:04 -0800

    Make Linux version of SS executable

--- tests/testthat/test-manipulate_EM.R
+++ tests/testthat/test-manipulate_EM.R
@@ -43,32 +43,32 @@ test_that("get_EM_dat works", {
   expect_equal(OM_dat$agecomp$obs[-rm_ind], new_dat$agecomp$obs)
 })
 #add tests for change_dat
-# # 
-# test_that("run_EM works", {
-#   skip_on_cran()
-#   df <- run_EM(EM_dir = file.path(temp_path, "cod"),  change_fcast = FALSE)
-#   fore <- r4ss::SS_readforecast(file.path(temp_path, "cod", "forecast.ss"))
-#   expect_equal(nrow(df), fore$Nforecastyrs)
-# 
-#   df2 <- run_EM(EM_dir = file.path(temp_path, "cod"),
-#                 change_fcast = TRUE,
-#                 nyrs_proj = 4)
-#   expect_equivalent(nrow(df2), 4)
-# 
-#   df3 <- run_EM(EM_dir = file.path(temp_path, "cod"),
-#                 set_use_par = TRUE)
-#   expect_equivalent(nrow(df3), 4)
-#   start <- r4ss::SS_readstarter(file.path(temp_path, "cod", "starter.ss"),
-#                                 verbose = FALSE)
-#   expect_equivalent(start$init_values_src, 1)
-# })
-# 
-# test_that("run_EM exits on error when it should", {
-#   # specify a directory that is not valid
-#   expect_error(run_EM(EM_dir = temp_path),
-#                "Please change to a directory containing a valid SS model.")
-#   # use change_fcast = TRUE with no value for nyrs_proj
-#   expect_error(run_EM(EM_dir = file.path(temp_path, "cod"),
-#                       change_fcast = TRUE),
-#                "argument \"nyrs_proj\" is missing, with no default")
-# })
+
+test_that("run_EM works", {
+  skip_on_cran()
+  df <- run_EM(EM_dir = file.path(temp_path, "cod"),  change_fcast = FALSE)
+  fore <- r4ss::SS_readforecast(file.path(temp_path, "cod", "forecast.ss"))
+  expect_equal(nrow(df), fore$Nforecastyrs)
+
+  df2 <- run_EM(EM_dir = file.path(temp_path, "cod"),
+                change_fcast = TRUE,
+                nyrs_proj = 4)
+  expect_equivalent(nrow(df2), 4)
+
+  df3 <- run_EM(EM_dir = file.path(temp_path, "cod"),
+                set_use_par = TRUE)
+  expect_equivalent(nrow(df3), 4)
+  start <- r4ss::SS_readstarter(file.path(temp_path, "cod", "starter.ss"),
+                                verbose = FALSE)
+  expect_equivalent(start$init_values_src, 1)
+})
+
+test_that("run_EM exits on error when it should", {
+  # specify a directory that is not valid
+  expect_error(run_EM(EM_dir = temp_path),
+               "Please change to a directory containing a valid SS model.")
+  # use change_fcast = TRUE with no value for nyrs_proj
+  expect_error(run_EM(EM_dir = file.path(temp_path, "cod"),
+                      change_fcast = TRUE),
+               "argument \"nyrs_proj\" is missing, with no default")
+})
