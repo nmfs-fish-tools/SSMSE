16628e867ea6978d4dac821dc4c1fb4af6e74524
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-07-03 12:09:53 -0700

    fix: binding for stats functions and ifelse was wrong
    *ifelse was elseif
    *two stats functions, rnorm and runif, needed stats::

--- R/recdevs.R
+++ R/recdevs.R
@@ -65,7 +65,7 @@ check_recdev_error <- function(length_recdev_seq, expected_length) {
 #' @param rec_autocorr_sd a vector of the standard deviation of estimated autocorrelation parameters with length equal to the order of the timelag
 #' @return a vector of recruitment deviations.
 calc_autoCor_rec_devs <- function(breaks, yrs, rec_autocorr_mean, rec_autocorr_sd) {
-  model_parms<-rnorm(n=length(rec_autocorr_mean),mean=rec_autocorr_mean,sd=rec_autocorr_sd)
+  model_parms<-stats::rnorm(n=length(rec_autocorr_mean),mean=rec_autocorr_mean,sd=rec_autocorr_sd)
   rec_dev_seq <- as.vector(stats::arima.sim(model=list(ma=model_parms), n=yrs))
   for (k in 1:(length(breaks) - 1)) {
     temp_rec_dev <- rec_dev_seq[(breaks[k] + 1):(breaks[k + 1])]
@@ -135,7 +135,7 @@ build_rec_devs <- function(yrs, nyrs_assess, scope, rec_dev_pattern, rec_dev_par
     for (i in 1:n_scenarios) {
       rec_autocorr_mean<-rec_autoCorr[[i]]$coef[1:4]
       rec_autocorr_sd<-sqrt(c(rec_autoCorr[[i]]$var.coef[1,1],rec_autoCorr[[i]]$var.coef[2,2], rec_autoCorr[[i]]$var.coef[3,3], rec_autoCorr[[i]]$var.coef[4,4]))
-      rec_autocorr_mean<-elseif(abs(rec_autocorr_mean)<(1.96*abs(rec_autocorr_sd)), 0, rec_autocorr_mean)  
+      rec_autocorr_mean<-ifelse(abs(rec_autocorr_mean)<(1.96*abs(rec_autocorr_sd)), 0, rec_autocorr_mean)  
       breaks <- unique(c(seq(0, yrs[i], rec_dev_pars[1]), yrs[i]))
       if (scope == 1) { 
         
