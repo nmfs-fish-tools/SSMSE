e4241e9a1d5cec9a8056c439939a40869f6c50ec
kathryn.doering <kathryn.doering@noaa.gov>
2020-07-22 16:47:37 -0700

    fix: adding new values from OM run to EM data set
    Discovered that sd's/Sample sizes are sometimes different; this is because the ss bootstrapping module adjust the sd/ sample size to include extra SD and the input variance from ctl file. Still need to think more about this wrt the EM - probably should turn off extra SD and input variance in the EM based on this?

--- R/manipulate_EM.R
+++ R/manipulate_EM.R
@@ -193,7 +193,31 @@ add_new_dat <- function(OM_dat,
       function(df, df_name, OM_dat) {
         OM_df <- OM_dat[[df_name]]
         OM_df[, 3] <- abs(OM_df[, 3]) # get rid of negative fleet values from OM
-        new_dat <- merge(df, OM_df, all.x = TRUE, all.y = FALSE)
+
+        by_val <- switch(df_name, 
+               "catch" = c("year", "seas", "fleet"), 
+               "CPUE" = c("year", "seas", "index"), 
+               "lencomp" = c("Yr", "Seas", "FltSvy", "Gender", "Part"),
+               "agecomp" = c("Yr", "Seas", "FltSvy", "Gender", "Part", "Ageerr", 
+                             "Lbin_lo", "Lbin_hi")
+               )
+        new_dat <- merge(df, OM_df, by = by_val, all.x = TRUE, all.y = FALSE)
+        # Sample sizes are likely different from user inputs if there is 
+        # variance adjustment values or extra SD. Keep the Nsamp.y value
+        # (from the bootstrap sample) To be consistent with other bootstrap
+        # model values
+        if("catch_se.y" %in% colnames(new_dat)) {
+          new_dat[["catch_se.x"]] <- NULL
+          colnames(new_dat)[which(colnames(new_dat) == "catch_se.y")] <- "catch_se"
+        }
+        if("se_log.y" %in% colnames(new_dat)) {
+          new_dat[["se_log.x"]] <- NULL
+          colnames(new_dat)[which(colnames(new_dat) == "se_log.y")] <- "se_log"
+        }
+        if("Nsamp.y" %in% colnames(new_dat)) {
+          new_dat[["Nsamp.x"]] <- NULL
+          colnames(new_dat)[which(colnames(new_dat) == "Nsamp.y")] <- "Nsamp"
+        }
         # warn if there were matches not found for OM_df, but remove to continue
         if (any(is.na(new_dat))) {
           warning("Some values specified in sample_struct (list component ", df_name,
