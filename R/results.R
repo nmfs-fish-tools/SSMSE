13b4ee6d5d3884a5f237baa3d53b12c9dba72043
k-doering-NOAA <kathryn.doering@noaa.gov>
2020-08-20 14:06:28 -0700

    feat: don't run parallel if will be very slow
    this is the case when all scenario files already exist.

--- R/results.R
+++ R/results.R
@@ -8,8 +8,8 @@
 #'  summaries. If left as NULL, the summaries will be extracted from all folders
 #'  in dir.
 #' @param run_parallel Option to use parallel processing. Defaults to FALSE.
-#'  Note that running in parallel will only be faster (and will be much slower)
-#'   if most of the scenarios do not yet have scenario-level summary files .
+#'  Note that running in parallel will only be faster if most of the scenarios
+#'  do not yet have scenario-level summary files .
 #' @param n_cores How many cores to use if running in parallel. If is NULL, 
 #'  defaults to n_cores available - 1 (also capped at one less than the number
 #'  of cores available - 1) 
@@ -29,22 +29,42 @@ SSMSE_summary_all <- function(dir = getwd(), scenarios = NULL,
     scenarios <- list.dirs(dir, full.names = FALSE, recursive = FALSE)
   }
   if (run_parallel) {
-    # setup and run parallel
-    if (is.null(n_cores)) n_cores <- detectCores() - 1
-    if (!is.null(n_cores)) n_cores <- min(max(n_cores, 1), (detectCores() - 1))
-    cl <- parallel::makeCluster(n_cores)
-    on.exit(stopCluster(cl))
-    result <- parLapply(cl = cl, X = scenarios, 
-                        fun = get_results_scenario, 
-                        directory = dir, 
-                        overwrite_files = TRUE)
-    ret <- get_results_all(directory = dir, user_scenarios = scenarios, 
-                           overwrite_files = FALSE, filename_prefix = "SSMSE")
-  } else {
-   ret <- ss3sim::get_results_all(directory = dir, user_scenarios = scenarios, 
-                                  overwrite_files = FALSE, 
-                                  filename_prefix = "SSMSE")
+    exists_scen_files <- lapply(scenarios, function(scen, dir) {
+      exists_file_list <- lapply(c("dq", "scalar", "ts"),
+        function(file, dir, scen) {
+        exists_file <- file.exists(
+          file.path(dir, scen, paste0("results_", file, "_", scen, ".csv")))
+      }, dir = dir, scen = scen)
+      if(all(unlist(exists_file_list) == TRUE)) {
+        return <- TRUE
+      } else {
+        return <- FALSE
+      }
+    }, dir = dir)
+    if(all(unlist(exists_scen_files) == TRUE)) {
+      warning("run_parallel is TRUE, but will take much longer than running in",
+              " serial because the scenario files already exist. Skipping ", 
+              "parallel processing. If you wish to create new scenario files",
+              " please delete them and rerun SSMSE_summary_all.")
+      use_parallel <- FALSE
+    } else {
+      use_parallel <- TRUE
+    }
+    if(use_parallel) {
+      # setup and run parallel
+      if (is.null(n_cores)) n_cores <- detectCores() - 1
+      if (!is.null(n_cores)) n_cores <- min(max(n_cores, 1), (detectCores() - 1))
+      cl <- parallel::makeCluster(n_cores)
+      on.exit(stopCluster(cl))
+      result <- parLapply(cl = cl, X = scenarios, 
+                          fun = get_results_scenario, 
+                          directory = dir, 
+                          overwrite_files = TRUE)
+    }
   }
+  ret <- ss3sim::get_results_all(directory = dir, user_scenarios = scenarios, 
+                                 overwrite_files = FALSE, 
+                                 filename_prefix = "SSMSE")
   ret
 }
 
