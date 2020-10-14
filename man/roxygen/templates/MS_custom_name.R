dff88356941d3f6204571499d02d9fceaf1feeaa
kathryn.doering <kathryn.doering@noaa.gov>
2020-10-14 10:26:55 -0700

    docs: run devtools::document

--- man/roxygen/templates/MS_custom_name.R
+++ man/roxygen/templates/MS_custom_name.R
@@ -1,3 +1,4 @@
 #' @param MS_custom_name The function name (available in the R envioronment) to
 #'  use if MS = 'custom' is used. Will not be used if a different management
 #'  strategy is used.
+
