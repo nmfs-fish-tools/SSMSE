54ac9193167aec0e61a85224a02cdd9529bdda6d
kathryn.doering <kathryn.doering@noaa.gov>
2021-01-07 16:12:25 -0800

    issue #25, dev: init comp data plot

--- R/results.R
+++ R/results.R
@@ -127,7 +127,6 @@ SSMSE_summary_iter <- function(dir) {
   res <- ss3sim::get_results_iter(dir_1_iter = dir)
 }
 
-
 #' Plot index data, expected values, and sampled data for 1 scenario
 #'
 #' Creates a plot that can be used to see how sampling lines up with 
@@ -215,6 +214,99 @@ plot_index_sampling <- function(dir = getwd()) {
     ggplot2::theme_classic()
 
   index_list <- list(index_dat = index_dat, index_plot = index_plot)
+}
+
+#' Plot comp data, expected values, and sampled data for 1 scenario
+#'
+#' Creates a plot that can be used to see how sampling lines up with 
+#' data and expected values for the index of abundance
+#' @param dir Path to the directory containing 1 scenario. Defaults to
+#'  the current working directory.
+#' @param comp_type Type of composition data, age or length. Defaults to age.
+#' @export
+#' @import ggplot2
+#' @author Kathryn Doering
+#' @return A list containing 2 components: 1) the ggplot object and 2) the
+#'  dataframe used to make the ggplot object
+plot_comp_sampling <- function(dir = getwd(), comp_type = c("agecomp", "lencomp")) {
+  
+  comp_type <- match.arg(as.character(comp_type), choices = c("agecomp", "lencomp"))
+  # get the iterations
+  iters <- list.dirs(dir, recursive = FALSE, full.names = FALSE)
+  scenario <- basename(dir)
+  # get the OM data values
+  om_name <- grep("OM", list.dirs(file.path(dir, iters[1]),
+                                  recursive = FALSE,
+                                  full.names = FALSE
+  ), value = TRUE)
+  assertive.types::assert_is_a_string(om_name)
+  # non-NULL compfile input provided and file exists
+  out_OM <- r4ss::SS_output(file.path(dir, as.character(iters[1]),
+                                      om_name),
+                            verbose = FALSE, 
+                            printstats = FALSE, 
+                            hidewarn = TRUE)
+  
+  if(comp_type == "agecomp") comp_dbase <- out_OM[["agedbase"]]
+  if(comp_type == "lencomp") comp_dbase <- out_OM[["lenbase"]]
+  comp_dbase <- type.convert(comp_dbase)
+  comp_dbase[["iteration"]] <- 1
+  comp_dbase[["scenario"]] <- scenario
+  comp_dbase[["model_run"]] <- "om"
+  
+  # Get the EM data ----
+  # get the EM init values
+  # em name is the same across iterations
+  em_name <- grep("EM_init$", list.dirs(file.path(dir, iters[1]),
+                                        recursive = FALSE,
+                                        full.names = FALSE
+  ), value = TRUE)
+  assertive.types::assert_is_a_string(em_name)
+  for (i in iters) {
+    tmp_out_EM <- r4ss::SS_output(file.path( dir, as.character(i),
+                                             em_name),
+                                  verbose = FALSE, 
+                                  printstats = FALSE, 
+                                  hidewarn = TRUE)
+    if(comp_type == "agecomp") tmp_comp_dbase <- tmp_out_EM[["agedbase"]]
+    if(comp_type == "lencomp") tmp_comp_dbase <- tmp_out_EM[["lendbase"]]
+    
+    tmp_comp_dbase[["iteration"]] <- i
+    tmp_comp_dbase[["scenario"]] <- scenario
+    tmp_comp_dbase[["model_run"]] <- em_name
+    comp_dbase <- rbind(comp_dbase, tmp_comp_dbase)
+  }
+  # get expected and observations in the same column
+  comp_dbase <- tidyr::gather(comp_dbase, "type_obs", "obs_value", 17:18) %>% 
+             dplyr::filter(model_run == "om" | (model_run == em_name & type_obs == "Obs"))
+  comp_dbase[["model_type_obs"]] <- paste0(comp_dbase[["model_run"]], "_", 
+                                           comp_dbase[["type_obs"]])
+  comp_dbase <- tidyr::spread(comp_dbase, model_type_obs, obs_value)
+  # Make the plot ----
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    # this is unlikely to be used, as  parallel is installed with base R.
+    warning(
+      "The package ggplot2 is not available.",
+      " Returning early with just the dataframe and not the plot."
+    )
+    return(list(comp_dat = comp_dbase, plot = NA))
+  }
+  if(comp_type == "agecomp") xlab_val <- "Age"
+  if(comp_type == "lencomp") xlab_val <- "Size Bins"
+  # need a loop for multiple fleets (I think we just want a plot per fleet)
+  # Need to add in better labels for sex
+  comp_plot <-  ggplot(comp_dbase, aes(x = Bin, y = om_Exp))+
+    geom_area(fill = "grey")+
+    geom_point(aes(y = om_Obs), color = "red", size = 4)+
+    geom_point(aes(y = cod_EM_init_Obs, shape = iteration), color = "black")+
+    facet_wrap(vars(Yr))+
+     scale_shape_manual(values = 
+       rep(15, length(unique(comp_dbase[["iteration"]]))))+
+    ylab("Proportion")+
+    xlab(xlab_val)+
+    theme_classic()
+
+  comp_list <- list(comp_dat = comp_dbase, plot = comp_plot)
 }
 
 #' get basic data to calculate performance metrics
