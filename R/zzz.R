38de94c6335b70241d17fd683ddf5e82c6eb21f1
k-doering-NOAA <k-doering-NOAA@users.noreply.github.com>
2023-09-08 15:28:57 +0000

    style and docs: run devtools::document() and styler::style_pkg()

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to SSMSE version ",
-  packageVersion(pkgname),
-  " using Stock Synthesis 3.30.18.")
+  packageStartupMessage(
+    "Welcome to SSMSE version ",
+    packageVersion(pkgname),
+    " using Stock Synthesis 3.30.18."
+  )
 }
