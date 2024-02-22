6d552a4377929c33cc0bbafb7cfe66cba4277048
k-doering-NOAA <kathryn.doering@noaa.gov>
2024-02-22 10:38:00 -0800

    fix: for multi area models
    Fleets only fish in 1 area, so this should work for all multiarea models. Thanks to @skylersagarese-NOAA for identifying this issue and fix!

--- R/extendOM.R
+++ R/extendOM.R
@@ -424,7 +424,12 @@ update_OM <- function(OM_dir,
           achieved_F <- F_achieved[F_achieved[, "year"] == catch_intended[i, "year"] &
             F_achieved[, "seas"] == catch_intended[i, "seas"] &
             F_achieved[, "fleet"] == catch_intended[i, "fleet"], "F"]
-
+          if(length(achieved_F) > 1) {
+            achieved_F <- achieved_F[achieved_F != 0]
+            if(length(achieved_F) > 1) {
+              stop("achieved_F has a length greater than 1. Contact the SSMSE developers for assistance.")
+            }
+          }
           if (intended_landings == 0) {
             target_F <- 0
           } else if (achieved_landings == 0) {
